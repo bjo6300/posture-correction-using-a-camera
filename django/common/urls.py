8dcbe4f289419bcf433978090b7a463ab0a8f108
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2022-03-15 01:10:01 +0900

    DMWY-47-아이디-찾기-UI-생성

--- django/common/urls.py
+++ django/common/urls.py
@@ -1,9 +1,19 @@
+from django import views
 from django.urls import path
+from unicodedata import name
 from django.contrib.auth import views as auth_views
+from . import views
 
 app_name = 'common'
 
 urlpatterns = [
+    
+    # 로그인 "{% url 'common:login' %}"
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
+
+    # 로그아웃 "{% url 'common:logout' %}"
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # 아이디 찾기 "{% url 'common:find_id' %}"
+    path('find_id/', views.find_id, name='find_id'),
 ]
--- django/common/views.py
+++ django/common/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from common import views
 
-# Create your views here.
+app_name = 'common'
+
+# 회원가입
+def find_id(request):
+    return render(request, 'common/find_id.html')
