d5aa75c67c7a81ea6110e8b87e88e565e8d52e5c
김주한 <83435602+juhan2103@users.noreply.github.com>
2022-03-25 18:27:37 +0900

    잘못된-자세-인식-코드-병합

--- pcp/posture_recognition.py
+++ pcp/posture_recognition.py
@@ -2,14 +2,9 @@ import cv2
 import time
 import modules.HolisticModule as hm
 from win10toast import ToastNotifier
-import math
-
-###################################################
-sensitivity = 8
-###################################################
 
 
-#fps = 1초당 프레임의 수
+# fps = 1초당 프레임의 수
 # privious time for fps
 pTime = 0
 # cerrent time for fps
@@ -30,15 +25,21 @@ jaw_bone_count = 0
 # 실제 턱 괴기 변수 초기 세팅
 real_jaw_bone_count = 0
 
+# 어깨 비대칭 변수 초기 세팅
+shoulder_count = 0
+
+# 실제 어깨 비대칭 변수 초기 세팅
+real_shoulder_count = 0
+
+# 양 어깨 높이 차이 변수
+shoulder_hd = 0
+
 while True:
     # default BGR img
     success, img = cap.read()
 
-    # mediapipe를 거친 이미지 생성 -> img
-    img = detector.findHolistic(img, draw=True)
-
     # 웹캠 이미지 반전
-    img = cv2.flip(img, 1)
+    img = detector.findHolistic(cv2.flip(img, 1), draw=True)
 
     # output -> list ( id, x, y, z) 32 개 좌표인데 예를 들면, (11, x, y, z)
     pose_lmList = detector.findPoseLandmark(img, draw=True)
@@ -61,15 +62,26 @@ while True:
         left_hand_len = detector.findPointDistance(152,20)
         right_hand_len = detector.findPointDistance(152, 17)
 
-        img1 = detector.drawPointDistance(152, 20, img, draw=True)
-        img2 = detector.drawPointDistance(152, 17, img, draw=True)
+        # 양 어깨의 관절점 사이의 거리를 이미지로 변경
+        shoulder_img = detector.drawShoulder(11,12,img,draw=True)
+
+        # 어깨의 y값 비교하여 양 쪽 어깨의 높이가 다르면 어깨 비대칭으로 인식
+        shoulder_hd = detector.findShoulder(11, 12)
+
+        left_hand_img = detector.drawPointDistance(152, 20, img, draw=True)
+        right_hand_img = detector.drawPointDistance(152, 17, img, draw=True)
 
         if left_hand_len < 105 or right_hand_len < 105:
             jaw_bone_count += 1
         else:
             jaw_bone_count = 0
 
-        # 100번 턱 괴기가 인식되면 알림을 제공한다.
+        if shoulder_hd >= 20:
+            shoulder_count += 1
+        else:
+            shoulder_count = 0
+
+        # 3초동안 턱 괴기 자세가 인식되면 알림을 제공한다.
         if jaw_bone_count > my_computer_fps * 3:
             print("WARNING - Please hands down")
             # win10toast 알림 제공
@@ -79,7 +91,16 @@ while True:
             jaw_bone_count = 0
 
             real_jaw_bone_count += 1
-        print("Length1 : {:.3f},   Length2 : {:.3f}".format(left_hand_len, right_hand_len))
+
+        if shoulder_count > my_computer_fps * 3:
+            print("WARNING - Keep your posture straight.")
+            toaster.show_toast("Asymmetric shoulders WARNING",
+                               f"Keep your posture straight.")
+            # 알림 제공 후 카운트를 다시 0으로 만듬
+            shoulder_count = 0
+
+            real_shoulder_count += 1
+        print("왼쪽 손 : {:.3f},   오른쪽 손 : {:.3f},    어깨 높이 차 : {:.1f}".format(left_hand_len, right_hand_len, shoulder_hd))
 
     # fps를 이미지 상단에 입력하는 로직
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
