9f0914e1e27d4166265b769bfe354b068cff4368
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2022-06-19 16:36:06 +0900

    DMWY-53-자세영상-DB-생성

--- django/posture/models.py
+++ django/posture/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class correction_video(models.Model):
+    video_index = models.AutoField(verbose_name='게시글 인덱스', primary_key=True)
+    posture = models.CharField(max_length=100, verbose_name='자세 이름')
+    title = models.CharField(max_length=200, verbose_name='제목')
+    video_link = models.CharField(max_length=1000, verbose_name='영상 주소')
+
+    def __str__(self):
+        return self.posture
+
+    class Meta:
+        db_table = 'correction_video'
+    
